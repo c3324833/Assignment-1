0ca6dbe442e7e18f1be5e5a242a1766d658ba285
Annie Simpson <annie.simpson@uon.edu.au>
2019-04-04 05:49:55 +0000

    End of Week 6 lab

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+
     printf("Please select an option: \n");\
     printf("a) Encrypt a message using rotation cipher with key given\n");
     printf("b) Decrypt a message using rotation cipher with key given\n");
@@ -8,10 +9,10 @@ int main() {
     printf("d) Decrypt a message using substitution cipher with substitutions given\n");
     printf("e) Decrypt a message using rotation cipher given text only\n");
     printf("f) Decrypt a message using substitution cipher given text only\n");
-    
+
     char c;
     scanf("%c", &c);
-    
+
     while(c < 'a' || c > 'f'){
         switch(c){
             case 'a': encryptRK();
@@ -29,29 +30,29 @@ int main() {
             default: printf("Unknown option %c\nPlease enter a, b, c, d, e or f\n");
         }
     }
-    
-    // Case 'a': Function prototype for encryptRK()
-    void encryptRK(char message, int key);
-    
-    int main() {
-        
-        char message;
+
+    // Case 'a': Function for encryptRK()
+    void encryptRK(char message[], int key); // Function prototype
+
+        char message[25];
         int key;
+        int i; // letter counter
         
+    void encryptRK(char message [], int key)
+    for(i = 0; i < message; ++i ){
+
         printf("Enter message to encrypt: ");
         scanf("%c", message);
-        
+
         printf("Enter rotation key: ");
         scanf("%d", &key);
-        
-        encryptRK(message, key);
-        
-        return 0;
+
+        encryptRK(message + key) %26;
     }
-    
-    void encryptRK(char message [], int key)
-   
-    
+
+
+     
+
+
 
   return 0;
-}
